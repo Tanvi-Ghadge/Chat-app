aaf61a26290e6534156648d95b575ce7acee2566
tanvi ghadge <tanvighadge27@gmail.com>
2025-03-20 23:20:18 +0530

    update backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,13 +25,7 @@ app.use(cors({ origin: allowedOrigin, credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
