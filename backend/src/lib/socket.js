6ca5e19768d760aa2b769e7ef09c11496cd554e2
tanvi ghadge <tanvighadge27@gmail.com>
2025-03-20 23:13:51 +0530

    Update axios API base URL to use environment variable

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,10 +4,12 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
-
+const originallowed = process.env.FRONTEND_URL || "http://localhost:5173";
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [originallowed],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
