670f5c4e513d066eb4f008e00582064db640f7d3
tanvi ghadge <tanvighadge27@gmail.com>
2025-03-21 02:07:58 +0530

    changes in the API and env variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,14 +16,15 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: process.env.NODE_ENV === "production", // Prevent XSS attacks in production
-    sameSite: "lax", // Prevent CSRF but allow cross-origin auth
+    sameSite: "none", // Prevent CSRF but allow cross-origin auth
     secure: process.env.NODE_ENV === "production", // Enable only in HTTPS
     path: "/", // Ensure it's available across the whole site
   });
