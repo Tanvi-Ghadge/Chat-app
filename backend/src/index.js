6c66ffad06ecb19e78b80ec16b6498f6c4da3f61
tanvi ghadge <tanvighadge27@gmail.com>
2025-03-30 15:12:50 +0530

    chnages in profile update

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+
 app.use(cookieParser());
 app.use(express.json({ limit: "20mb" }));  
 app.use(express.urlencoded({ limit: "20mb", extended: true }));  
